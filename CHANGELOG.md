5669e620288ae2b4f4037c5939520ed5e0a85158
ShyeChern <chern-97@hotmail.com>
2021-11-06 10:08:21 +0800

    fix mp3 format

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,11 @@
 # CHANGELOG
+
+## v1.0.2 (2021-11-06)
+### Fix
+* Fix mp3 file format
+### Misc
+* Update scss code
+
 ## v1.0.1 (2021-10-23)
 ### Fix
 * Fix typo
--- src/pages/api/download/index.js
+++ src/pages/api/download/index.js
@@ -1,9 +1,12 @@
 const ytdl = require('ytdl-core');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 export default async function handler(req, res) {
 	try {
 		if (req.method === 'GET') {
-      // get title and download mp3
+			// get title and download mp3
 			const url = req.query.url;
 			const info = await ytdl.getBasicInfo(url);
 			res.writeHead(200, {
@@ -11,10 +14,15 @@ export default async function handler(req, res) {
 					info.videoDetails.title
 				)}.mp3"`,
 			});
-			ytdl(url, {
+
+			// get stream
+			const stream = ytdl(url, {
 				filter: 'audioonly',
 				format: 'mp3',
-			}).pipe(res);
+			});
+			// ouput stream as mp3 file
+			ffmpeg(stream).format('mp3').output(res).run();
+
 		} else {
 			res.status(404).send();
 		}
